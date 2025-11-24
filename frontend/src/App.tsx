a335b5df7f76c4429554133903ae112b3cfb4217
Ewertton Goncalves <dev.ewerttongoncalves@gmail.com>
2025-11-24 16:14:13 -0300

    feat: Adiciona configuração inicial frontend React + Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-700 flex items-center justify-center">
+      <h1 className="text-4xl text-white font-bold">
+        Tailwind OK ✅
+      </h1>
+    </div>
+  )
+}
+
+export default App
